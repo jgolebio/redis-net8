2b2227efa14874ebf6d9ac172a1ccf7d41865598
Jakub Gołębiowski <j.g.golebiowski@gmail.com>
2024-01-09 22:44:13 +0100

    add redis cache

---
+++ src/services/categories-api/CacheService/RedisCacheService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace categories_api.CacheService
+{
+    public class RedisCacheService
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public RedisCacheService(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public T? GetCashedData<T>(string key)
+        {
+            var jsonData = _distributedCache.Get(key);
+            if (jsonData is null)
+                return default(T);
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+
+        public void AddCashedData<T>(string key, T data, TimeSpan duration)
+        {
+            var options = new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = duration
+            };
+
+            var jsonData = JsonSerializer.Serialize(data);
+            _distributedCache.SetString(key, jsonData, options);
+        }
+    }
+}
--- src/services/categories-api/Program.cs
+++ src/services/categories-api/Program.cs
@@ -1,6 +1,25 @@
+using categories_api.CacheService;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddStackExchangeRedisCache(o =>
+{
+    o.Configuration = builder.Configuration["RedisCacheUrl"];
+});
+builder.Services.AddSingleton<RedisCacheService>();
 var app = builder.Build();
 
-app.MapGet("/categories", () => new string[] { "one", "two", "three", "four" });
+app.MapGet("/categories", (RedisCacheService redisCacheService) =>
+{
+    var categoriesKey = "Redis_Categories";
+
+    var categories = redisCacheService.GetCashedData<string[]>(categoriesKey);
+    if (categories is null)
+    {
+        categories = new string[] { "one", "two", "three", "four" };
+        redisCacheService.AddCashedData(categoriesKey, categories, TimeSpan.FromSeconds(60));
+    }
+
+    return categories;
+});
 
 app.Run();
